e2fe92da2e79715db8db432d0ad9277bb97ec406
Zdeněk Drahoš <drahoszdenek@gmail.com>
2015-09-21 15:22:15 +0200

    CI - verbose mode

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 composer install
-phpqa --analyzedDir ./ --buildDir ./var/CI --ignoredDirs=bin,vendor
+phpqa --verbose --analyzedDir ./ --buildDir ./var/CI --ignoredDirs=bin,vendor
 bin/phpunit --log-junit ./var/CI/junit.xml --testdox-text ./var/CI/testdox.txt --testdox-html ./var/CI/testdox.html
