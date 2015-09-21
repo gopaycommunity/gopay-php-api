c9b004e2e1ed7a7b138a6ade896fe95a8fbf1f0f
Zdeněk Drahoš <drahoszdenek@gmail.com>
2015-09-21 09:44:48 +0200

    Http - encode data

--- src/Http/RequestHelper.php
+++ src/Http/RequestHelper.php
@@ -2,6 +2,8 @@
 
 namespace GoPay\Http;
 
+use GoPay\Browser;
+
 class RequestHelper
 {
     public function getBaseApiUrl($isProductionMode)
@@ -11,5 +13,14 @@ class RequestHelper
             false => 'https://gw.sandbox.gopay.com/api/'
         ];
         return $urls[(bool) $isProductionMode];
+    }
+
+    public function encodeData(array $headers, array $data)
+    {
+        if ($data) {
+            $encoder = $headers['Content-Type'] == Browser::FORM ? 'http_build_query' : 'json_encode';
+            return $encoder($data);
+        }
+        return '';
     }
 }
--- tests/Http/RequestHelperTest.php
+++ tests/Http/RequestHelperTest.php
@@ -2,6 +2,8 @@
 
 namespace GoPay\Http;
 
+use GoPay\Browser;
+
 class RequestHelperTest extends \PHPUnit_Framework_TestCase
 {
     private $request;
@@ -22,6 +24,23 @@ class RequestHelperTest extends \PHPUnit_Framework_TestCase
         return [
             'test' => [false, 'https://gw.sandbox.gopay.com/api/'],
             'prod' => [true, 'https://gate.gopay.cz/api/'],
+        ];
+    }
+
+    /** @dataProvider provideContentTypeAndData */
+    public function testShouldEncodeData($contentType, $data, $expectedBody)
+    {
+        $headers = ['Content-Type' => $contentType];
+        assertThat($this->request->encodeData($headers, $data), identicalTo($expectedBody));
+    }
+
+    public function provideContentTypeAndData()
+    {
+        return [
+            'empty form' => [Browser::FORM, [], ''],
+            'empty json' => [Browser::JSON, [], ''],
+            'form' => [Browser::FORM, ['key' => 'value'], 'key=value'],
+            'json' => [Browser::JSON, ['key' => 'value'], '{"key":"value"}'],
         ];
     }
 }
