c68b50bc3c51e841885636cb49bae29128fbdc14
Zdeněk Drahoš <drahoszdenek@gmail.com>
2015-09-21 09:20:12 +0200

    Auth - extract OAuth tests

---
+++ src/OAuth2.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace GoPay;
+
+class OAuth2
+{
+    private $config;
+    private $browser;
+
+    private $accessToken;
+
+    public function __construct(array $config, Browser $b)
+    {
+        $this->config = $config;
+        $this->browser = $b;
+    }
+
+    public function getAccessToken()
+    {
+        if (!$this->accessToken) {
+            $this->authorize();
+        }
+        return $this->accessToken;
+    }
+
+    private function authorize()
+    {
+        $response = $this->api(
+            'oauth2/token',
+            [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/x-www-form-urlencoded',
+                'auth' => [$this->config['clientID'], $this->config['clientSecret']]
+            ],
+            ['grant_type' => 'client_credentials', 'scope' => $this->config['scope']]
+        );
+        if ($response->hasSucceed()) {
+            $this->accessToken = $response->json['access_token'];
+        }
+    }
+
+    private function api($urlPath, $headers, array $data)
+    {
+        return $this->browser->postJson(
+            "https://gw.sandbox.gopay.com/api/{$urlPath}",
+            $headers,
+            $data
+        );
+    }
+}
---
+++ tests/OAuth2Test.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace GoPay;
+
+class OAuth2Test extends \PHPUnit_Framework_TestCase
+{
+    private $config = [
+        'clientID' => 'irrelevant id',
+        'clientSecret' => 'irrelevant secret',
+        'scope' =>PaymentScope::ALL
+    ];
+
+    private $browser;
+    private $auth;
+
+    protected function setUp()
+    {
+        $this->browser = $this->prophesize('GoPay\Browser');
+        $this->auth = new OAuth2($this->config, $this->browser->reveal());
+    }
+
+    /** @dataProvider provideAccessToken */
+    public function testShouldRequestAccessTokenOnce($statusCode, array $jsonResponse, $expectedCalls, $expectedToken)
+    {
+        $apiResponse = new Response;
+        $apiResponse->statusCode = $statusCode;
+        $apiResponse->json = $jsonResponse;
+
+        $this->browser->postJson(
+            'https://gw.sandbox.gopay.com/api/oauth2/token',
+            [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/x-www-form-urlencoded',
+                'auth' => [$this->config['clientID'], $this->config['clientSecret']],
+            ],
+            ['grant_type' => 'client_credentials', 'scope' => $this->config['scope']]
+        )->shouldBeCalledTimes($expectedCalls)->willReturn($apiResponse);
+
+        assertThat($this->auth->getAccessToken(), is($expectedToken));
+        assertThat($this->auth->getAccessToken(), is($expectedToken));
+    }
+
+    public function provideAccessToken()
+    {
+        return [
+            'success' => [200, ['access_token' => 'new token', 'expires_in' => 100], 1, 'new token'],
+            'failure' => [400, ['error' => 'access_denied'], 2, nullValue()]
+        ];
+    }
+
+}
