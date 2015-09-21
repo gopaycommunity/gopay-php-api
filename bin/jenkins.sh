e7aed125f5778b4d03180dcfbbb759555b8bcce7
Zdeněk Drahoš <drahoszdenek@gmail.com>
2015-09-21 06:54:01 +0200

    CI - log junit and testdox

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 composer install
-bin/phpunit
 phpqa --analyzedDir ./ --buildDir ./var/CI --ignoredDirs=bin,vendor
+bin/phpunit --log-junit ./var/CI/junit.xml --testdox-text ./var/CI/testdox.txt --testdox-html ./var/CI/testdox.html
