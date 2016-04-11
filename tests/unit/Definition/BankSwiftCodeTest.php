739126fc52f5d7377b37d91aeb7f1467977a4a01
Zdeněk Drahoš <drahoszdenek@gmail.com>
2016-04-11 07:25:42 +0200

    Enums - test swift code length

---
+++ tests/unit/Definition/BankSwiftCodeTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace GoPay\Definition\Payment;
+
+class BankSwiftCodeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSwiftCodeHasEightOrTwelveCharacters()
+    {
+        $reflection = new \ReflectionClass('GoPay\Definition\Payment\BankSwiftCode');
+        foreach ($reflection->getConstants() as $code) {
+            $this->assertSwiftCodeLength($code);
+        }
+    }
+
+    private function assertSwiftCodeLength($code)
+    {
+        $expectedLength = $code == BankSwiftCode::ERA ? 12 : 8;
+        assertThat(strlen($code), is($expectedLength));
+    }
+}
